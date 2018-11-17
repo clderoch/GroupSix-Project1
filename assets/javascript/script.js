4d668cde655b715f36b141bf7c8579d56c5f7bbb
Dustin Crawford <crawforddustins@yahoo.com>
2018-11-17 13:35:26 -0600

    Test

---
+++ assets/javascript/script.js
@@ -0,0 +1,43 @@
+function searchBreweryFinder(brewery) {
+
+  var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + brewery + "&sort=-postal_code";;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(response)
+    $("#brewerys").empty();
+
+    for (var i = 0; i < 10; i++) {
+      console.log(response[i]);
+      var brewery = $("<div>");
+
+      brewery.attr({
+        "class": 'brewery',
+        "data-name": response[i].name,
+        "data-addr": response[i].street,
+        "data-city": response[i].city,
+        "data-state": response[i].state,
+        "data-postal": response[i].postal_code,
+        "data-long": response[i].longitude,
+        "data-lat": response[i].latitude,
+        "data-URL": response[i].website_url
+      });
+
+      var breweryName = $("<h1>").html("<a target='_blank' href=" + response[i].website_url + ">" + response[i].name + "</a>");
+      var breweryURL = $("<a>").attr("href", response[i].website_url);
+      var breweryAddress = $("<h3>").text(response[i].street + " || " + response[i].postal_code + " || " + response[i].brewery_type);
+
+      brewery.append(breweryName, breweryURL, breweryAddress);
+      $("#brewerys").append(brewery);
+    }
+  });
+}
+
+$("#select-brewery").on("click", function (event) {
+  event.preventDefault();
+  var inputBrewery = $("#brewery-input").val().trim();
+  searchBreweryFinder(inputBrewery);
+});
